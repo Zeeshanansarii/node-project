490c5d851ac550ea51d3a0ab5201d0f7798b04b4
zeeshan-ansari <zeeshan.ansari@mogiio.com>
2024-03-05 23:34:44 +0530

    add

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("<h1> Hello, Welcome to IIT Bombay Computer Science ");
+});
+
+
+const usersRoute = require('./routes/users');
+const productRoute = require('./routes/product');
+
+app.use('./users', usersRoute);
+app.use('./product', productRoute);
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on the port ${port}`);
+});
